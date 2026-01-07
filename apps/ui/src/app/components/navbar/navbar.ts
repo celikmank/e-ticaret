7fbc785ecf067b03ef45854d913dc104f0ed3211
Keremcelikman <kerem.celikman@delta-yazilim.com>
2026-01-07 10:39:38 +0300

    Kategori listesini navbar da gösterme

--- apps/ui/src/app/components/navbar/navbar.ts
+++ apps/ui/src/app/components/navbar/navbar.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { httpResource } from '@angular/common/http';
+import { Component, computed } from '@angular/core';
+import { RouterLink } from '@angular/router'; 
+import { CategoryModel } from '@e-ticaret/shared/models/Category.model';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
+  imports: [RouterLink], 
   templateUrl: './navbar.html'
 })
-export class Navbar {}
+export class Navbar {
+  readonly result = httpResource<CategoryModel[]>(() => `api/categories`);
+  readonly data = computed(() => this.result.value() ?? []);
+}
